8cbe55a9d498cf8f2c600455d6c079cb461af1d8
Paul Martens <paul.martens@divotion.com>
2020-03-04 08:28:11 +0100

    Set up app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'styles/app.scss';
+
+const App: React.FC = () => (
+	<main>
+		<h1>Yolt Game Night</h1>
+	</main>
+);
 
 export default App;
